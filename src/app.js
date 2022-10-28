5750118ae8eef5f125044bbec3626fa99bf6d4c7
Mukul Dahiya <1029mukul38@gmail.com>
2022-10-28 13:30:39 +0530

    successFullCcC2

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-require("./Db/connMongoDb");
+// require("./Db/connMongoDb");
 const LoginFormDetails = require("./models/apniKalmSchema");
 const path = require("path");
 const hbs =require("hbs");
